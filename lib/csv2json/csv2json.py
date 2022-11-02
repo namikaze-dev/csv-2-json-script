4af4d358182c731877fbdd8ec0ac92e2378e965d
usmansadik <usmansadik96@gmail.com>
2022-11-02 23:43:12 +0100

    Implement method to transform object to json template

--- lib/csv2json/csv2json.py
+++ lib/csv2json/csv2json.py
@@ -1,32 +1,43 @@
 import csv
 
-def load(file_name):
-	dataset = list(csv.reader(open(file_name)))
-	header = dataset.pop(0)
-	return jsonify(dataset, header)
+def load(file_name, **kwargs):
+    dataset = list(csv.reader(open(file_name)))
+    header = dataset.pop(0)
+    
+    if (kwargs.get('smoothify') and kwargs.get('sep')):
+        header = smoothify(header, sep=kwargs['sep'])
+    elif (kwargs.get('smoothify')):
+        header = smoothify(header)
+    
+    return jsonify(dataset, header)
 
 
 def jsonify(dataset, header):
-	return list(map(lambda row: transform(header, row), dataset))
+    return list(map(lambda row: transform(header, row), dataset))
 
 def transform(header, row):
-	processed = {}
-	for i in range(len(header)):
-		if i >= len(row):
-			processed[header[i]] = ''
-		else:
-			value = parse_int(row[i])
-			processed[header[i]] = value
-	return processed
+    processed = {}
+    for i in range(len(header)):
+        if i >= len(row):
+            processed[header[i]] = ''
+        else:
+            value = parse_int(row[i])
+            processed[header[i]] = value
+    return processed
 
 def parse_int(str_value):
-	try:
-		return int(str_value)
-	except:
-		return parse_bool(str_value)
+    try:
+        return int(str_value)
+    except:
+        return parse_bool(str_value)
 
 def parse_bool(str_value):
-	try:
-		return eval(str_value.capitalize())
-	except:
-		return str_value
+    try:
+        return eval(str_value.capitalize())
+    except:
+        return str_value
+    
+    
+def smoothify(header, sep='-'):
+    key = lambda text: sep.join(text.lower().split())
+    return list(map(key, header))
--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-from re import I
 import sys
+import csv
+
+from hashlib import sha256
 
 from lib.csv2json import csv2json
-from hashlib import sha256
 
 ARGS = sys.argv
 FILENAME = ARGS[1]
+DATASET = list(csv.reader(open(FILENAME)))
+JSON_DATASET = csv2json.load(FILENAME, smoothify=True, sep='_')
+HEADER = DATASET.pop(0)
+ROWS = DATASET
 
 
+def main():
+    pass
+
+
+def json_transform(row):
+    json_object = {
+        "format": "CHIP-0007",
+        "name": row.get('name') or row.get('filename') or row.get('file_name') or row.get('nft_name') or "",
+        "description": row.get('description') or "",
+        "minting_tool": "Team x",
+        "sensitive_content": False,
+        "series_number": row.get('series_number'),
+        "series_total": 526,
+        "attributes": [{
+            "trait_type": "gender",
+            "value": row.get('gender') or ""
+        }],
+        "collection": {
+            "name":
+            "Zuri NFT Tickets for Free Lunch",
+            "id":
+            "b774f676-c1d5-422e-beed-00ef5510c64d",
+            "attributes": [{
+                "type": "description",
+                "value": "Rewards for accomplishments during HNGi9."
+            }]
+        }
+    }
+    return json_object
+
+
+if __name__ == '__main__':
+    main()
