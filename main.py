f8f2727062b5759193512e517fdf3ed52e3561cb
usmansadik <usmansadik96@gmail.com>
2022-11-03 15:42:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,20 +11,32 @@ ARGS = sys.argv
 FILENAME = ARGS[1]
 OUTPUT_DIR_NAME = 'output'
 DATASET = list(csv.reader(open(FILENAME)))
-JSON_DATASET = csv2json.load(FILENAME, smoothify=True, sep='_')
 HEADER = DATASET.pop(0)
-ROWS = DATASET
+ROWS = list(filter(lambda row: row != [] and row[0] != '', DATASET))
 
 
 def main():
-    try:
-        os.mkdir(OUTPUT_DIR_NAME)
-    except Exception:
-        pass
+    create_folder(OUTPUT_DIR_NAME)
+    HASHES = {}
     
-    HASHES = engine(JSON_DATASET)
+    for team_name, rows in groupify(ROWS).items():
+        json_dataset = csv2json.jsonify(rows, csv2json.smoothify(HEADER))
+        current_hashes = engine(json_dataset, team_name=team_name, path=team_name)
+        HASHES.update(current_hashes)
+    
     write_csv(f'{file_base_name(FILENAME)}.output.csv', ROWS, HEADER, HASHES)
     print('SUCCESS!')
+    
+    
+def groupify(rows):
+    grouped_rows, curr_team = {}, None
+    for row in rows:
+        if 'team' in row[0].lower():
+            curr_team = row[0]
+            grouped_rows[curr_team] = []
+        else:
+            grouped_rows[curr_team].append(row)
+    return grouped_rows
     
 
 def write_csv(filename, rows, header, hashes, col_name='Sha256 Json Hash'):
@@ -36,30 +48,31 @@ def write_csv(filename, rows, header, hashes, col_name='Sha256 Json Hash'):
             csv_writer.writerow(row + [hashes.get(row[0])])
     
     
-def engine(json_dataset):
+def engine(json_dataset, path='', team_name='Team X'):
     hashes = {}
     for row in json_dataset:
-        filename = os.path.join(OUTPUT_DIR_NAME, f'{valid_available_name(row)}.json')
+        create_folder(os.path.join(OUTPUT_DIR_NAME, path))
+        filename = os.path.join(OUTPUT_DIR_NAME, path, f'{valid_available_name(row)}.json')
         with open(filename, 'w') as json_file:
-            json_object = json_transform(row)
+            json_object = json_transform(row, team_name)
             json.dump(json_object, json_file, indent=4)
         with open(filename, 'rb') as json_file:
-            key = str(row.get('series_number' or row.get('series-number')))
+            key = str(row.get('series_number') or row.get('series-number'))
             hashes[key] = sha256(json_file.read()).hexdigest() 
     return hashes
 
 
-def json_transform(row):
+def json_transform(row, mint_tool):
     attributes_object = parse_attributes(row)
     
     json_object = {
         "format": "CHIP-0007",
         "name": valid_available_name(row) or "",
         "description": row.get('description') or "",
-        "minting_tool": "Team x",
+        "minting_tool": mint_tool,
         "sensitive_content": False,
-        "series_number": row.get('series_number'),
-        "series_total": 526,
+        "series_number": row.get('series_number') or row.get('series-number'),
+        "series_total": 420,
         "attributes": [{
             "trait_type": "gender",
             "value": row.get('gender') or ""
@@ -94,10 +107,10 @@ def file_base_name(filename):
 
 
 def parse_attributes(json_object):
-    str_attributes = json_object.get('attributes') or json_object.get('attributes-_hair._eyes._teeth._clothing._accessories._expression._strength._weakness')
+    str_attributes = json_object.get('attributes')
     if not str_attributes:
-        print('NOTE: There are Invalid "attributes" field(s) in your csv file!')
-        return {'':''}
+        # print('NOTE: There are Invalid "attributes" field(s) in your csv file!')
+        return {'': ''}
     if ',' in str_attributes:
         attributes = str_attributes.split(',')
     else:
@@ -109,6 +122,13 @@ def parse_attributes(json_object):
 def make_attributes_object(attributes):
     processed = list(map(lambda atr: atr.split(':'), attributes))
     return {key.strip(): val.strip() for key, val in processed}
+
+
+def create_folder(folder_name):
+    try:
+        os.mkdir(folder_name)
+    except Exception:
+        pass
     
     
 if __name__ == '__main__':
