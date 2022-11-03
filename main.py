9909db7ef8e8919d9014a58892cbc6c8779d27d1
usmansadik <usmansadik96@gmail.com>
2022-11-03 22:10:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,33 +8,55 @@ from hashlib import sha256
 from lib.csv2json import csv2json
 
 ARGS = sys.argv
-FILENAME = ARGS[1]
-OUTPUT_DIR_NAME = 'output'
-DATASET = list(csv.reader(open(FILENAME)))
-HEADER = DATASET.pop(0)
-ROWS = list(filter(lambda row: row != [] and row[0] != '', DATASET))
+FILENAME = None
+OUTPUT_DIR_NAME = None
+DATASET = None
+HEADER = None
+ROWS = None
 
 
 def main():
-    create_folder(OUTPUT_DIR_NAME)
+    load_globals()
+    create_dir(OUTPUT_DIR_NAME)
+    
     HASHES = {}
     
     for team_name, rows in groupify(ROWS).items():
         json_dataset = csv2json.jsonify(rows, csv2json.smoothify(HEADER))
-        current_hashes = engine(json_dataset, team_name=team_name, path=team_name)
+        current_hashes = json_hash_handler(json_dataset, sub_dir_path=team_name)
         HASHES.update(current_hashes)
     
     write_csv(f'{file_base_name(FILENAME)}.output.csv', ROWS, HEADER, HASHES)
     print('SUCCESS!')
     
     
+def load_globals():
+    global FILENAME
+    global OUTPUT_DIR_NAME
+    global HEADER
+    global ROWS
+    global DATASET
+    
+    try:
+        FILENAME = ARGS[1]
+    except Exception:
+        print('filename argument missing!\npass the name of the file as an argument')
+        sys.exit(-1)
+        
+    OUTPUT_DIR_NAME = 'output'
+    DATASET = list(csv.reader(open(FILENAME)))
+    HEADER = DATASET.pop(0)
+    ROWS = list(filter(lambda row: row != [], DATASET))
+    
+     
 def groupify(rows):
     grouped_rows, curr_team = {}, None
     for row in rows:
         if 'team' in row[0].lower():
             curr_team = row[0]
-            grouped_rows[curr_team] = []
+            grouped_rows[curr_team] = [row]
         else:
+            row[0] = curr_team
             grouped_rows[curr_team].append(row)
     return grouped_rows
     
@@ -42,36 +64,36 @@ def groupify(rows):
 def write_csv(filename, rows, header, hashes, col_name='Sha256 Json Hash'):
     new_header = header + [col_name]
     with open(os.path.join(OUTPUT_DIR_NAME, filename), 'w', encoding='UTF-8', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(new_header)
+        writer = csv.writer(csv_file)
+        writer.writerow(new_header)
         for row in rows:
-            csv_writer.writerow(row + [hashes.get(row[0])])
+            key = int(row[1])
+            writer.writerow(row + [hashes.get(key)])
     
     
-def engine(json_dataset, path='', team_name='Team X'):
+def json_hash_handler(json_dataset, sub_dir_path):
     hashes = {}
     for row in json_dataset:
-        create_folder(os.path.join(OUTPUT_DIR_NAME, path))
-        filename = os.path.join(OUTPUT_DIR_NAME, path, f'{valid_available_name(row)}.json')
-        with open(filename, 'w') as json_file:
-            json_object = json_transform(row, team_name)
+        create_dir(os.path.join(OUTPUT_DIR_NAME, sub_dir_path))
+        json_filename = os.path.join(OUTPUT_DIR_NAME, sub_dir_path, row.get('filename') + '.json')
+        with open(json_filename, 'w') as json_file:
+            json_object = json_transform(row)
             json.dump(json_object, json_file, indent=4)
-        with open(filename, 'rb') as json_file:
-            key = str(row.get('series_number') or row.get('series-number'))
+        with open(json_filename, 'rb') as json_file:
+            key = row.get('series-number')
             hashes[key] = sha256(json_file.read()).hexdigest() 
     return hashes
 
 
-def json_transform(row, mint_tool):
+def json_transform(row):
     attributes_object = parse_attributes(row)
-    
     json_object = {
         "format": "CHIP-0007",
-        "name": valid_available_name(row) or "",
+        "name": row.get('name') or "",
         "description": row.get('description') or "",
-        "minting_tool": mint_tool,
+        "minting_tool": row.get('team-names'),
         "sensitive_content": False,
-        "series_number": row.get('series_number') or row.get('series-number'),
+        "series_number": row.get('series-number') or 0,
         "series_total": 420,
         "attributes": [{
             "trait_type": "gender",
@@ -98,10 +120,6 @@ def json_transform(row, mint_tool):
     return json_object
 
 
-def valid_available_name(row):
-    return row.get('filename') or row.get('file_name') or row.get('nft_name') or row.get('name')
-
-
 def file_base_name(filename):
     return filename.removesuffix('.csv')
 
@@ -109,22 +127,25 @@ def file_base_name(filename):
 def parse_attributes(json_object):
     str_attributes = json_object.get('attributes')
     if not str_attributes:
-        # print('NOTE: There are Invalid "attributes" field(s) in your csv file!')
+        print('NOTE: There are Invalid/Empty "attributes" field(s) in your csv file!')
         return {'': ''}
-    if ',' in str_attributes:
-        attributes = str_attributes.split(',')
-    else:
-        attributes = str_attributes.split('.')
+    attributes = str_attributes.split(';')
     attributes_object = make_attributes_object(attributes)
     return attributes_object
     
 
 def make_attributes_object(attributes):
-    processed = list(map(lambda atr: atr.split(':'), attributes))
-    return {key.strip(): val.strip() for key, val in processed}
+    try:
+        filtered = list(filter(lambda atr: bool(atr), attributes))
+        processed = list(map(lambda atr: atr.split(':'), filtered))
+        return {key.strip(): val.strip() for key, val in processed}
+    except ValueError as error:
+        print(f'Invalid attributes field: {filtered}\nerror:', error)
+        sys.exit(-1)
+        
 
 
-def create_folder(folder_name):
+def create_dir(folder_name):
     try:
         os.mkdir(folder_name)
     except Exception:
